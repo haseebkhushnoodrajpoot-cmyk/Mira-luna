29f010e53655a6e0f1c13f4f99c6e59e7f6b082e
haseebkhushnoodrajpoot-cmyk <haseebkhushnoodrajpoot@gmail.com>
2026-08-20 19:42:45 +0500

    Fix vite config: add defineConfig and publicDir

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  publicDir: 'public',   // <-- this copies public/ to dist/
   build: {
     outDir: 'dist',
     rollupOptions: {
